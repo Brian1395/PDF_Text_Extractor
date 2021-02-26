183880bb9e88b80b47d97b277cbaca954395699a
Brian1395 <briandalegoodell@gmail.com>
2021-02-26 15:05:16 -0600

    Update Letter_Extractor.py
    You can now isolate bits of the image by clicking. Next, I'm planning to add some ease of use changes and the prompts for each character to learn

--- Letter_Extractor.py
+++ Letter_Extractor.py
@@ -15,11 +15,12 @@ images = convert_from_path('Source.pdf',poppler_path = r"C:\Users\brian\Document
 #images = convert_from_bytes(open('Source.pdf', 'rb').read())
 
 img = images[1]
+img = img.resize((int(img.width/2),int(img.height/2)))
 #img.show()
 
 ## Creates a Tkinter instance where you can choose the corners of the letter
 root = Tk()      
-canvas = Canvas(root, width = 10000, height = 10000)      
+canvas = Canvas(root, width = 950, height = 800)      
 canvas.pack()
 tkimg = ImageTk.PhotoImage(img)
 canvas.create_image(10,10, anchor=NW, image=tkimg)
@@ -30,19 +31,38 @@ def firstcorner(eventorigin):
     x0 = eventorigin.x
     y0 = eventorigin.y
     print(x0,y0)
-    w.bind("<Button 1>",secondcorner)
+    root.bind("<Button 1>",secondcorner)
 
 # Determine the origin by clicking
 def secondcorner(eventorigin):
     global x1,y1
     x1 = eventorigin.x
     y1 = eventorigin.y
-    print(x0,y0)
-    #w.bind("<Button 1>",getextentx)
+    print("second", x1,y1)
+    cutimage()
+    #
 
 #Cuts the selected section of the image
 def cutimage():
-    
+    if(x0 < x1):
+        left = x0
+        right = x1
+    else:
+        left = x1
+        right = x0
+
+    if(y0<y1):
+        upper = y0
+        lower = y1
+    else:
+        upper = y1
+        lower = y0
+
+        
+    section = img.crop((left, upper, right, lower))
+    section.show()
+
+    root.bind("<Button 1>",firstcorner)
     
 #mouseclick event
 root.bind("<Button 1>",firstcorner)
