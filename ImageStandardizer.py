6ca6707e06bb18f3a1aef63cccb45ab30124fbc9
Brian1395 <briandalegoodell@gmail.com>
2021-04-13 23:27:56 -0500

    Update ImageStandardizer.py
    Made it so the images keep their aspect ratio and are set on a consistently sized white background

--- ImageStandardizer.py
+++ ImageStandardizer.py
@@ -9,7 +9,12 @@ class ImageStandardizer:
     def convert_from_file(self, location):
         self.input_image = Image.open(location)
 
-        self.output_img = self.input_image.resize(self.output_size)
-        self.output_img = self.output_img.convert('L')
+        self.input_image.thumbnail(self.output_size, Image.ANTIALIAS)
+        self.output_img = Image.new('L', (28, 28), (255)) #I assume it'll be better for the neural net if the backgrounds are consistently white, but maybe the algorthim would improve if it could identify the bufferspace. An experiement for another time?
+        
+        self.x_offset = int((28 - self.input_image.width) / 2)
+        self.y_offset = int((28 - self.input_image.height) / 2)
+        
+        self.output_img.paste(self.input_image, (self.x_offset, self.y_offset))#img.crop((-10,0,28,28)), )
 
         return(self.output_img)
